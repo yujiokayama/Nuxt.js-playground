962ac9badb88cb14b1ff8b3e6856d58438b23e81
yuji-okayama <y-okayama@tribeck.jp>
2020-12-03 13:05:12 +0900

    add scss

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,7 @@ export default {
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [],
+  css: [{ src: '~/assets/scss/index.scss', lang: 'scss' }],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [],
